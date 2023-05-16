7a00d232b6e66bfd93aeca5db309c936a987ba33
Sadık Sünbül <jsjsqwe12@gmail.com>
2023-05-16 10:35:48 +0300

    Asp.NET Core 5.0 - Tuple Nesne Post Etme ve Yakalama

--- ders3/Controllers/ProductController.cs
+++ ders3/Controllers/ProductController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ders3.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System;
 
 namespace ders3.Controllers
 {
@@ -10,13 +13,22 @@ namespace ders3.Controllers
         }
 
         #region Asp.NET Core 5.0 - Tuple Nesne Post Etme ve Yakalama
+        //Egerkı bınf mekanızmasında tuple turde bır nesne kullanıyorsak bu tuple nesnesındekı datalar/degerler/nesneler / object olusturulup verilmesı gerekmektedır 
         public IActionResult Get()
         {
             return View();
         }
+        [HttpPost]
         public IActionResult Create()
+        {
+            var tuple = (new Product(), new User()); //null olmaması ıcın yaptık 
+            return View(tuple);
+        }
+        [HttpPost]
+        public IActionResult Create([Bind(Prefix = "Item1")]Product product, [Bind(Prefix = "Item2")] User user) //burada ıtem ıle verılmelı dıger tarafta ıstedıgın kadar ezmıs  ol burada ıselmez
         {
             return View();
         }
+        #endregion
     }
 }
