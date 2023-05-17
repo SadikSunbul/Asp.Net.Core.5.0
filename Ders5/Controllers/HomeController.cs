f1582069e87788458f43e7c9c42347f47f7b0282
Sadık Sünbül <jsjsqwe12@gmail.com>
2023-05-17 17:52:57 +0300

    Asp.NET Core 5.0 - _ViewStart ve _ViewImports Dosyaları Nelerdir?

---
+++ Ders5/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ders5.Controllers
+{
+    public class HomeController : Controller
+    {
+        #region Asp.NET Core 5.0 - Layout Yapılanması Nedir? RenderBody ve RenderSection Fonksiyonları Nelerdir?
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Sayfa1()
+        {
+            return View();
+        }
+        public IActionResult Sayfa2()
+        {
+            return View();
+        }
+
+        #endregion
+        #region Asp.NET Core 5.0 - _ViewStart ve _ViewImports Dosyaları Nelerdir?
+        //_ViewStart dosyası:Asıl amacı tüm Viewler de kullanılması yapılması gereken ortak calısmaların yapıldıgı view dir .cshtml dır Bir nevi tüm viewlerin atasıdır dıyebılırız(ilk once bu render edılır)Viewlerde isim cok onemlıdır Views klasoru altında _ViewStart.cshtml olusturulması gerekır .Genellikle tüm viewlarda ortak kullancagımız Layout tanımlaması bu dosya içerisinde gerceklestırılır
+
+        //_ViewImports dosyası:Razor sayfalrı ıcın kutuphane ve namespace tanımlamalarını sayfa sayfa farklı tanımlamaktansa ortak merkezı olarak tanımlamamızı saglayan bır dosyadır . Views Klasörü altında _ViewImports.cshtml isminde olusturulmalıdır
+
+        #endregion
+    }
+}
---
+++ Ders5/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace Ders5.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string İsim { get; set; }
+        
+    }
+}
