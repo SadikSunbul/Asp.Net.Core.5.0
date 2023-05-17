6ba26065aeb49157a496e2dfb9393b3160dd2c58
Sadık Sünbül <jsjsqwe12@gmail.com>
2023-05-17 21:56:55 +0300

    son krıtıkler yapıldı

--- Ders6/ViewComponents/PersonellerViewComponent.cs
+++ Ders6/ViewComponents/PersonellerViewComponent.cs
@@ -7,8 +7,10 @@ namespace Ders6.ViewComponents
 {
     public class PersonellerViewComponent:ViewComponent
     {
-        public IViewComponentResult Invoke() //Tasarlanan viewcomponent cagrılıp render edıldıgınde ıcerısınde calısmasını ıstedıgımız kodların bu ımzada bır metodun ıcerısıne yerlestırmelıyız ısmı boyle olucak farklı bı ısım olursa calısmaz
+        //[NonViewComponent] buradsı vıewcomponent degıl demıs olduk burada 
+        public IViewComponentResult Invoke(int id) //Tasarlanan viewcomponent cagrılıp render edıldıgınde ıcerısınde calısmasını ıstedıgımız kodların bu ımzada bır metodun ıcerısıne yerlestırmelıyız ısmı boyle olucak farklı bı ısım olursa calısmaz
         {
+            //(int id)  id degerınde dıger taraftan gelen verıyı yakalıya bılıyoruz
             List<Personel> data = new List<Personel>
             {
                 new Personel{Adi="Sadık",Soyadı="Sünbül"},
@@ -24,6 +26,11 @@ namespace Ders6.ViewComponents
          */
         /*
          Genelde kullanıcagımız yer Shared ıcerısınde Componnets Onunda ıcerısınde Ilgılı calss ısmı (Personeller) onunda ıcerısınde Default -->View() veya Ahe-->View("Ahmet") demke gerekır 
+         */
+        /*
+         critikler:
+        burada sadece gets seklınde calısmakta bır post alamaz burası ısteklerı karsılamaz yanı burası 
+        burada bı form tasarladıysanı bunu ilgili controlra yonlendırmek gerekır 
          */
     }
 }
